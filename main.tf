cf90e68043eb278a429efc2b860b8661bbf08994
root <root@ip-172-31-35-18.ap-south-1.compute.internal>
2023-01-21 20:16:54 +0000

    splited main.tf in to portions

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   cloud {
     organization = "isuru-corp"
     workspaces {
-      name = "isuru-corp-ws1"
+      name = "simple-aws-env
     }
   }
   required_providers {
@@ -13,13 +13,4 @@ terraform {
   }
   required_version = ">= 1.2.0"
 }
-provider "aws" {
-  region = "ap-south-1"
-}
-resource "aws_instance" "app_server" {
-  ami           = "ami-0cca134ec43cf708f"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "IsuruEd"
-  }
-}
+

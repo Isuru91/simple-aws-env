a819f067339975447ac961346cf52ac293a769d0
root <root@ip-172-31-35-18.ap-south-1.compute.internal>
2023-01-21 21:00:46 +0000

    remove all the lines in resource block on vm.tf to destroy the created vm. but not the vm.tf file

--- vm.tf
+++ vm.tf
@@ -1,8 +1,1 @@
-resource "aws_instance" "web_server" {
-  ami           = "ami-0cca134ec43cf708f"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "development_server"
-    Date = "2023-01-22"
-  }
 }
